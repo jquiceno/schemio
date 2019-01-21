adc8bc44465f0cabd139dc4526ea64746b8eed0a
Jquiceno <loncuster@gmail.com>
2019-01-21 11:04:17 -0500

    Mejoras y actualizaciones en validación de datos
    - Se agrega soporte para la validación de datos de tipo array y object
    
    - Se agrega soporte para parametros default y value en el template de 
    esquema
    
    - Se agregan nuevos tests

--- src/lib/schema/index.js
+++ src/lib/schema/index.js
@@ -4,7 +4,7 @@ import Boom from 'boom'
 import Joi from 'joi'
 import defaults from 'defaults'
 
-const types = ['string', 'number', 'email']
+const types = ['string', 'number', 'email', 'array', 'object']
 
 class Schema {
   constructor (schema) {
@@ -27,11 +27,30 @@ class Schema {
         const item = schema[key]
         let valid = false
 
-        if (item.require) {
+        if (item.require && (!item.default && !item.value)) {
           valid = Joi.validate(data[key], Joi.required())
 
           if (valid.error) {
             throw Boom.badRequest(`The fiel ${key} is required`)
+          }
+        }
+
+        if (item.default && !data[key]) {
+          newData[key] = schema[key].default
+          delete data[key]
+
+          return
+        }
+
+        if (typeof item.value !== 'undefined') {
+          if (typeof item.value === 'function') {
+            newData[key] = item.value(data[key], _data)
+          } else {
+            newData[key] = item.value
+          }
+
+          if (newData[key]) {
+            return delete data[key]
           }
         }
 
@@ -42,6 +61,10 @@ class Schema {
             valid = Joi.validate(data[key], Joi.number())
           } else if (item.type === 'email') {
             valid = Joi.validate(data[key], Joi.string().email())
+          } else if (item.type === 'array') {
+            valid = Joi.validate(data[key], Joi.array())
+          } else if (item.type === 'object') {
+            valid = Joi.validate(data[key], Joi.object())
           }
 
           if (valid.error) {
--- test/validschema.test.js
+++ test/validschema.test.js
@@ -2,6 +2,7 @@
 
 import test from 'ava'
 import { Schema } from '../src'
+import moment from 'moment'
 
 test.beforeEach(t => {
   t.context.schema = {
@@ -15,6 +16,25 @@ test.beforeEach(t => {
     },
     age: {
       type: 'number'
+    },
+    city: {
+      default: 'Medellin'
+    },
+    state: {
+      value: (value, data) => {
+        return 'Antioquia'
+      }
+    },
+    created: {
+      value: (value, data) => {
+        return moment().unix()
+      }
+    },
+    idioms: {
+      type: 'array'
+    },
+    skills: {
+      type: 'object'
     }
   }
 
@@ -22,8 +42,70 @@ test.beforeEach(t => {
     name: 'John Quiceno',
     country: 'Colombia',
     email: 'test@gmail.com',
-    age: 29
+    age: 29,
+    idioms: ['English', 'Spanish', 'Italian'],
+    skills: {
+      php: true,
+      nodejs: true,
+      go: true,
+      python: true
+    }
   }
+})
+
+test('Validating obect data', t => {
+  const data = t.context.data
+  const schema = t.context.schema
+
+  const validData = Schema.validate(data, schema)
+
+  const errData = t.throws(() => {
+    data.skills = 'php, node'
+    return Schema.validate(data, schema)
+  })
+
+  t.is(typeof validData.skills, 'object')
+  t.deepEqual(errData.output.statusCode, 400)
+  t.regex(errData.output.payload.message, /Error, the skills field must be a object/)
+})
+
+test('Validating array data', t => {
+  const data = t.context.data
+  const schema = t.context.schema
+
+  const validData = Schema.validate(data, schema)
+
+  const errData = t.throws(() => {
+    data.idioms = {
+      English: true
+    }
+    return Schema.validate(data, schema)
+  })
+
+  t.is(Array.isArray(validData.idioms), true)
+  t.deepEqual(errData.output.statusCode, 400)
+  t.regex(errData.output.payload.message, /Error, the idioms field must be a array/)
+})
+
+test('Validating default callback function', t => {
+  const data = t.context.data
+  const schema = t.context.schema
+
+  const validData = Schema.validate(data, schema)
+
+  t.deepEqual(validData.name, data.name)
+  t.deepEqual(validData.state, 'Antioquia')
+  t.is(typeof validData.created, 'number')
+})
+
+test('Validating default single data', t => {
+  const data = t.context.data
+  const schema = t.context.schema
+
+  const validData = Schema.validate(data, schema)
+
+  t.deepEqual(validData.name, data.name)
+  t.deepEqual(validData.city, schema.city.default)
 })
 
 test('Validating number data', t => {
